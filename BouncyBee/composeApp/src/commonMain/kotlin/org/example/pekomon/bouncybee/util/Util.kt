6c32d180c1ce775420f766f9833aeb42a0d3294c
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-03-22 20:06:38 +0200

    Font resource getter function to utils

---
+++ BouncyBee/composeApp/src/commonMain/kotlin/org/example/pekomon/bouncybee/util/Util.kt
@@ -0,0 +1,10 @@
+package org.example.pekomon.bouncybee.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import bouncybee.composeapp.generated.resources.Res
+import bouncybee.composeapp.generated.resources.chewy_regular
+import org.jetbrains.compose.resources.Font
+
+@Composable
+fun ChewyFontFamily() = FontFamily(Font(Res.font.chewy_regular))
