ad9bf24734d7affe30d2caa6431a26672af10d06
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-03-22 18:15:31 +0200

    update material to material3

--- BouncyBee/composeApp/src/commonMain/kotlin/org/example/pekomon/bouncybee/App.kt
+++ BouncyBee/composeApp/src/commonMain/kotlin/org/example/pekomon/bouncybee/App.kt
@@ -1,6 +1,6 @@
 package org.example.pekomon.bouncybee
 
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
