a7bf92db18efb042e4d3d71634525bc9fe10fb47
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-03-23 16:53:42 +0200

    Show background image edge-to-edge

--- BouncyBee/composeApp/src/commonMain/kotlin/org/example/pekomon/bouncybee/App.kt
+++ BouncyBee/composeApp/src/commonMain/kotlin/org/example/pekomon/bouncybee/App.kt
@@ -1,13 +1,31 @@
 package org.example.pekomon.bouncybee
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import bouncybee.composeapp.generated.resources.Res
+import bouncybee.composeapp.generated.resources.background
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.background),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
     }
 }
