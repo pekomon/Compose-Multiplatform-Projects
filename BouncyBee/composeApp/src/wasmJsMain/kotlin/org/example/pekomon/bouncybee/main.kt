088487b409b2d71ee121318e3bfddbda5918d168
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-05-03 16:42:58 +0300

    Koin initializtion for wasm

--- BouncyBee/composeApp/src/wasmJsMain/kotlin/org/example/pekomon/bouncybee/main.kt
+++ BouncyBee/composeApp/src/wasmJsMain/kotlin/org/example/pekomon/bouncybee/main.kt
@@ -3,9 +3,11 @@ package org.example.pekomon.bouncybee
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.example.pekomon.bouncybee.di.initializeKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initializeKoin()
     ComposeViewport(document.body!!) {
         App()
     }
