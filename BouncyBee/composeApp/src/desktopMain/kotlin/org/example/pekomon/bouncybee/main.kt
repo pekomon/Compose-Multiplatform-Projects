2fa2a21327545e27ac57617533cbc85f9ace1174
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-05-03 16:41:56 +0300

    Koin init for desktop

--- BouncyBee/composeApp/src/desktopMain/kotlin/org/example/pekomon/bouncybee/main.kt
+++ BouncyBee/composeApp/src/desktopMain/kotlin/org/example/pekomon/bouncybee/main.kt
@@ -2,8 +2,10 @@ package org.example.pekomon.bouncybee
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.example.pekomon.bouncybee.di.initializeKoin
 
 fun main() = application {
+    initializeKoin()
     Window(
         onCloseRequest = ::exitApplication,
         title = "Bouncy Bee",
