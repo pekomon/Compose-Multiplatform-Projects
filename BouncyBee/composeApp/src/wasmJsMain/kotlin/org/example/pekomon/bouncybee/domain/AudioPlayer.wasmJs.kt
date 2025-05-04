cbf8e54338b1919c85160a6c569df6865047ed55
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-05-04 20:19:09 +0300

    Empty platform specific implementations of audio players

---
+++ BouncyBee/composeApp/src/wasmJsMain/kotlin/org/example/pekomon/bouncybee/domain/AudioPlayer.wasmJs.kt
@@ -0,0 +1,24 @@
+package org.example.pekomon.bouncybee.domain
+
+actual class AudioPlayer {
+    actual fun playGameOverSound() {
+    }
+
+    actual fun playJumpSound() {
+    }
+
+    actual fun playFallingSound() {
+    }
+
+    actual fun stopFallingSound() {
+    }
+
+    actual fun playBackgroundMusic() {
+    }
+
+    actual fun stopBackgroundMusic() {
+    }
+
+    actual fun release() {
+    }
+}
