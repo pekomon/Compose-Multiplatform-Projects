a825a30872ce32b5c1f83c93ef083453565d16b4
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-05-04 20:40:05 +0300

    Android impl of audioplayer (with temp local resources)

--- BouncyBee/composeApp/src/androidMain/kotlin/org/example/pekomon/bouncybee/domain/AudioPlayer.android.kt
+++ BouncyBee/composeApp/src/androidMain/kotlin/org/example/pekomon/bouncybee/domain/AudioPlayer.android.kt
@@ -1,25 +1,71 @@
 package org.example.pekomon.bouncybee.domain
 
+import android.content.Context
+import android.media.SoundPool
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import bouncybee.composeapp.generated.resources.Res
+import org.example.pekomon.bouncybee.R
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-actual class AudioPlayer {
+actual class AudioPlayer(
+    context: Context
+) {
+    private val loopingPlayer = ExoPlayer.Builder(context).build()
+    @OptIn(ExperimentalResourceApi::class)
+    private val mediaItems = soundResList.map {
+        MediaItem.fromUri(Res.getUri(it))
+    }
+
+    private val soundPool = SoundPool
+        .Builder()
+        .setMaxStreams(3)
+        .build()
+
+    private val jumpSound = soundPool.load(context, R.raw.jump, 2)
+    private val fallingSound = soundPool.load(context, R.raw.falling, 1)
+    private var fallingSoundId = 0
+    private val gameOverSound = soundPool.load(context, R.raw.game_over, 1)
+
+    init {
+        loopingPlayer.prepare()
+    }
+
     actual fun playGameOverSound() {
+        stopFallingSound()
+        soundPool.play(gameOverSound, 1f, 1f, 0, 0, 1f)
     }
 
     actual fun playJumpSound() {
+        stopFallingSound()
+        soundPool.play(jumpSound, 1f, 1f, 0, 0, 1f)
     }
 
     actual fun playFallingSound() {
+        fallingSoundId = soundPool.play(fallingSound, 1f, 1f, 0, 0, 1f)
     }
 
     actual fun stopFallingSound() {
+        soundPool.stop(fallingSoundId)
     }
 
     actual fun playBackgroundMusic() {
+        loopingPlayer.repeatMode = Player.REPEAT_MODE_ONE
+        loopingPlayer.setMediaItem(mediaItems[2])
+        loopingPlayer.play()
     }
 
     actual fun stopBackgroundMusic() {
+        loopingPlayer.pause()
+        playGameOverSound()
     }
 
     actual fun release() {
+        loopingPlayer.stop()
+        loopingPlayer.clearMediaItems()
+        loopingPlayer.release()
+        soundPool.release()
     }
 }
