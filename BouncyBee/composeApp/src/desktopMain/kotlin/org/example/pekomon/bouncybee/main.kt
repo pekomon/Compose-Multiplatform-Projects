d13a5cd51ce35ece917ce53f031f4e9f07f07876
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-05-09 22:09:41 +0300

    Fixed size for desktop app

--- BouncyBee/composeApp/src/desktopMain/kotlin/org/example/pekomon/bouncybee/main.kt
+++ BouncyBee/composeApp/src/desktopMain/kotlin/org/example/pekomon/bouncybee/main.kt
@@ -1,6 +1,8 @@
 package org.example.pekomon.bouncybee
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import org.example.pekomon.bouncybee.di.initializeKoin
 
@@ -9,6 +11,11 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Bouncy Bee",
+        state = WindowState(
+            width = 1200.dp,
+            height = 800.dp
+        ),
+        resizable = false
     ) {
         App()
     }
