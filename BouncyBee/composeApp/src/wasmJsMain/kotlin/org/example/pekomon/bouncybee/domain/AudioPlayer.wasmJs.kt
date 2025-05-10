23c88b40f3aacfe3924ae904081c60e132cb05eb
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-05-10 17:40:27 +0300

    Audio playing functions for wasm

--- BouncyBee/composeApp/src/wasmJsMain/kotlin/org/example/pekomon/bouncybee/domain/AudioPlayer.wasmJs.kt
+++ BouncyBee/composeApp/src/wasmJsMain/kotlin/org/example/pekomon/bouncybee/domain/AudioPlayer.wasmJs.kt
@@ -1,7 +1,12 @@
 package org.example.pekomon.bouncybee.domain
 
+import org.w3c.dom.Audio
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class AudioPlayer {
+
+    private val audioElements = mutableMapOf<String, Audio>()
+
     actual fun playGameOverSound() {
     }
 
@@ -21,5 +26,42 @@ actual class AudioPlayer {
     }
 
     actual fun release() {
+    }
+
+    private fun stopSound(fileName: String) {
+        audioElements[fileName]?.let {
+            it.pause()
+            it.currentTime = 0.0
+        }
+    }
+
+    private fun stopAllSounds() {
+        audioElements.values.forEach {
+            it.pause()
+            it.currentTime = 0.0
+        }
+    }
+
+    private fun playSound(
+        fileName: String,
+        playInLoop: Boolean = false
+    ) {
+        val audio = audioElements[fileName] ?: createAudioElement(fileName).also { audioElements[fileName] = it }
+        audio.loop = playInLoop
+        audio.play().catch {
+            println("Error playing sound: $fileName")
+            it
+        }
+    }
+
+
+    private fun createAudioElement(filename: String): Audio {
+        val path = "src/commonMain/composeResources/files/$filename"
+        return Audio(path).apply {
+            onerror = { _: JsAny?, _: String, _: Int, _: Int, _: JsAny? ->
+                println("Error loading audio file: $path")
+                null
+            }
+        }
     }
 }
