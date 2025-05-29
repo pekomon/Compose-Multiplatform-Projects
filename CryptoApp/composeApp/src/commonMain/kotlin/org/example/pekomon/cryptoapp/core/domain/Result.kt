097f2d16a0c49054546da6d9ef940c264795ada2
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-05-29 17:36:26 +0300

    Add extension functions for Result

--- CryptoApp/composeApp/src/commonMain/kotlin/org/example/pekomon/cryptoapp/core/domain/Result.kt
+++ CryptoApp/composeApp/src/commonMain/kotlin/org/example/pekomon/cryptoapp/core/domain/Result.kt
@@ -2,6 +2,39 @@ package org.example.pekomon.cryptoapp.core.domain
 
 sealed interface Result<out D, out E: Error> {
     data class Success<out D>(val data: D) : Result<D, Nothing>
-    data class Failure<out E: Error>(val error: E) : Result<Nothing, E>
+    data class Error<out E: org.example.pekomon.cryptoapp.core.domain.Error>(val error: E) : Result<Nothing, E>
 
 }
+
+inline fun <T, E: Error, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
+    return when (this) {
+        is Result.Success -> Result.Success(map(data))
+        is Result.Error -> Result.Error(error)
+    }
+}
+
+fun <T, E: Error> Result<T, E>.asEmptyDataResult(): EmptyResult<E> {
+    return map { }
+}
+
+inline fun <T, E: Error> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, E> {
+    return when (this) {
+        is Result.Success -> {
+            action(data)
+            this
+        }
+        is Result.Error -> this
+    }
+}
+
+inline fun <T, E: Error> Result<T, E>.onError(action: (E) -> Unit): Result<T, E> {
+    return when (this) {
+        is Result.Success -> this
+        is Result.Error -> {
+            action(error)
+            this
+        }
+    }
+}
+
+typealias EmptyResult<E> = Result<Unit, E>
