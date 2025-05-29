f57ae5226448710f946155b4a585405ca72760e9
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-05-29 21:14:46 +0300

    Setup shared http client factory

---
+++ CryptoApp/composeApp/src/commonMain/kotlin/org/example/pekomon/cryptoapp/core/network/HttpClientFactory.kt
@@ -0,0 +1,36 @@
+package org.example.pekomon.cryptoapp.core.network
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.headers
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object HttpClientFactory {
+    fun create(engine: HttpClientEngine): HttpClient {
+        return HttpClient(engine) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+            install(HttpTimeout) {
+                socketTimeoutMillis = 20_000L
+                requestTimeoutMillis = 20_000L
+            }
+            install(HttpCache)
+            defaultRequest {
+                headers { append("x-access-token", "XXX") }
+                contentType(ContentType.Application.Json)
+            }
+        }
+    }
+}
