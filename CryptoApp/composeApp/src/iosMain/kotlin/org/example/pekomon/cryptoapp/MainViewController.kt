26778e5b9fc31b4eecfc1dcb2152a5df49603a98
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-06-08 12:11:55 +0300

    Init Koin for iOS

--- CryptoApp/composeApp/src/iosMain/kotlin/org/example/pekomon/cryptoapp/MainViewController.kt
+++ CryptoApp/composeApp/src/iosMain/kotlin/org/example/pekomon/cryptoapp/MainViewController.kt
@@ -1,5 +1,10 @@
 package org.example.pekomon.cryptoapp
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.example.pekomon.cryptoapp.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) { App() }
