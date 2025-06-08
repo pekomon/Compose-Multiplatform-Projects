aa58fc39d5df9ea864d31567928e18912583b9f5
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-06-08 19:46:51 +0300

    CoinsListViewModel + related stuff to Koin module

--- CryptoApp/composeApp/src/commonMain/kotlin/org/example/pekomon/cryptoapp/di/Module.kt
+++ CryptoApp/composeApp/src/commonMain/kotlin/org/example/pekomon/cryptoapp/di/Module.kt
@@ -1,10 +1,18 @@
 package org.example.pekomon.cryptoapp.di
 
 import io.ktor.client.HttpClient
+import org.example.pekomon.cryptoapp.coins.data.remote.impl.KtorCoinsRemoteDataSource
+import org.example.pekomon.cryptoapp.coins.domain.GetCoinDetailsUseCase
+import org.example.pekomon.cryptoapp.coins.domain.GetCoinsListUseCase
+import org.example.pekomon.cryptoapp.coins.domain.api.CoinsRemoteDataSource
+import org.example.pekomon.cryptoapp.coins.presentation.CoinsListViewModel
 import org.example.pekomon.cryptoapp.core.network.HttpClientFactory
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 fun initKoin(config: KoinAppDeclaration? = null) =
@@ -20,4 +28,10 @@ expect val platformModule: Module
 
 val sharedModule = module {
     single<HttpClient> { HttpClientFactory.create(get()) }
+
+    // coins list
+    viewModel { CoinsListViewModel(get()) }
+    singleOf(::GetCoinsListUseCase)
+    singleOf(::KtorCoinsRemoteDataSource).bind<CoinsRemoteDataSource>()
+    singleOf(::GetCoinDetailsUseCase)
 }
