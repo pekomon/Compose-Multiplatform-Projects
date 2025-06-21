3f815fd25349b2f0149638ecdfd7c726cb576f34
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-06-21 21:31:43 +0300

    DAO class for portfolio coin

---
+++ CryptoApp/composeApp/src/commonMain/kotlin/org/example/pekomon/cryptoapp/portfolio/data/local/PortfolioDao.kt
@@ -0,0 +1,21 @@
+package org.example.pekomon.cryptoapp.portfolio.data.local
+
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+interface PortfolioDao {
+
+    @Upsert
+    suspend fun insert(portfolioCoinEntity: PortfolioCoinEntity)
+
+    @Query("SELECT * FROM PortfolioCoinEntity ORDER BY timestamp DESC")
+    fun getAllOwnedCoins(): Flow<List<PortfolioCoinEntity>>
+
+    @Query("SELECT * FROM PortfolioCoinEntity WHERE coinId = :coinId")
+    suspend fun getCoinById(coinId: String): PortfolioCoinEntity?
+
+    @Query("DELETE FROM PortfolioCoinEntity WHERE coinId = :coinId")
+    suspend fun deletePortfolioItem(coinId: String)
+
+}
