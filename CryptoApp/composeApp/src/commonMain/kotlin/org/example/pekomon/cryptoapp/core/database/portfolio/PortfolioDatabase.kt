b4210ebca5ce88c69e0417025d2ab6a5513f11fc
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-07-03 20:43:21 +0300

    Include user balance dao to db

--- CryptoApp/composeApp/src/commonMain/kotlin/org/example/pekomon/cryptoapp/core/database/portfolio/PortfolioDatabase.kt
+++ CryptoApp/composeApp/src/commonMain/kotlin/org/example/pekomon/cryptoapp/core/database/portfolio/PortfolioDatabase.kt
@@ -4,8 +4,13 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import org.example.pekomon.cryptoapp.portfolio.data.local.PortfolioCoinEntity
 import org.example.pekomon.cryptoapp.portfolio.data.local.PortfolioDao
+import org.example.pekomon.cryptoapp.portfolio.data.local.UserBalanceDao
+import org.example.pekomon.cryptoapp.portfolio.data.local.UserBalanceEntity
 
-@Database(entities = [PortfolioCoinEntity::class], version = 1)
+@Database(
+    entities = [PortfolioCoinEntity::class, UserBalanceEntity::class], version = 2)
 abstract class PortfolioDatabase : RoomDatabase() {
     abstract fun portfolioDao(): PortfolioDao
+    abstract fun userBalanceDao(): UserBalanceDao
+
 }
