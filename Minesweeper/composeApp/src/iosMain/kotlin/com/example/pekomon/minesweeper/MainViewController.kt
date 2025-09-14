df688aa562b381e2728db39c4bfcb298ef98c8da
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-09-14 11:42:47 +0300

    Spotless automatic fixes

--- Minesweeper/composeApp/src/iosMain/kotlin/com/example/pekomon/minesweeper/MainViewController.kt
+++ Minesweeper/composeApp/src/iosMain/kotlin/com/example/pekomon/minesweeper/MainViewController.kt
@@ -1,5 +1,5 @@
 package com.example.pekomon.minesweeper
 
 import androidx.compose.ui.window.ComposeUIViewController
-
+@Suppress("ktlint:standard:function-naming") // Allow PascalCase for this factory function
 fun MainViewController() = ComposeUIViewController { App() }
