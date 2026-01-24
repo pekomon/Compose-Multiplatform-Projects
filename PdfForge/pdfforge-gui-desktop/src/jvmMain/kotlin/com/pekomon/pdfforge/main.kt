4c214c9391fdcb6fbc606f08d6a94251258c0d1b
Pekomon <5183385+pekomon@users.noreply.github.com>
2026-01-24 19:50:37 +0200

    M10: set macOS app menu name

--- PdfForge/pdfforge-gui-desktop/src/jvmMain/kotlin/com/pekomon/pdfforge/main.kt
+++ PdfForge/pdfforge-gui-desktop/src/jvmMain/kotlin/com/pekomon/pdfforge/main.kt
@@ -3,11 +3,15 @@ package com.pekomon.pdfforge
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "PdfForge",
-    ) {
-        App()
+fun main() {
+    System.setProperty("apple.awt.application.name", "PdfForge")
+    System.setProperty("com.apple.mrj.application.apple.menu.about.name", "PdfForge")
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "PdfForge",
+        ) {
+            App()
+        }
     }
 }
